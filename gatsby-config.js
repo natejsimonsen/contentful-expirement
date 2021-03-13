06ad2ee8bd0a8b8277b2612535b05e8e1f0d21b2
Nate Simonsen <natejsimonsen@gmail.com>
2021-03-12 19:23:05 -0800

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-console.log(process.env)
 module.exports = {
+  pathPrefix: "/contentful-expirement",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
